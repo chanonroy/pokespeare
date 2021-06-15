67d726e6e96edf1f221dce13a4cdbe7f0e987396
Chanon Roy <chanonroy@gmail.com>
2021-06-15 09:18:49 +0100

    add labels

--- src/resolvers/ListResolver.ts
+++ src/resolvers/ListResolver.ts
@@ -9,11 +9,15 @@ class CreateListInput {
 
 @Resolver()
 export class ListResolver {
-  @Mutation(() => List)
+  @Mutation(() => List, {
+    description: "Add pokemon to the user's saved list",
+  })
   async savePokemonToList(@Arg("input") input: CreateListInput): Promise<any> {
     return List.create(input).save();
   }
-  @Mutation(() => List)
+  @Mutation(() => List, {
+    description: "Remove pokemon from the user's saved list",
+  })
   async removePokemonFromList(
     @Arg("input") input: CreateListInput
   ): Promise<any> {
