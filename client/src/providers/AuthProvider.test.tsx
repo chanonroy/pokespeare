7108506d39b8814595ae8f2200012dcd9c72d37b
Chanon Roy <chanonroy@gmail.com>
2021-06-20 21:25:48 +0100

    add sample context test

--- client/src/providers/AuthProvider.test.tsx
+++ client/src/providers/AuthProvider.test.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { renderHook } from '@testing-library/react-hooks'
+import React, { useContext } from 'react'
+import AuthProvider, { AuthContext } from './AuthProvider'
+import { mocked } from 'ts-jest/utils'
+import { getAccessToken } from '../utils/token'
+import { ReactElement } from 'react'
+
+jest.mock('../utils/token')
 
 describe('Auth Provider', () => {
-  it.todo('restores session when access token is present')
+  it('restores session when access token is present', () => {
+    mocked(getAccessToken).mockReturnValue('accessToken')
+    const wrapper = ({ children }: { children: ReactElement }) => (
+      <AuthProvider>{children}</AuthProvider>
+    )
+    const { result } = renderHook(() => useContext(AuthContext), { wrapper })
+
+    expect(result.current.loggedIn).toBe(true)
+  })
   it.todo('handles login action')
   it.todo('handles logout action')
 })
